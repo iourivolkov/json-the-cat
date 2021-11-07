89fd600f6bf1ed61d5c7acdaebff7df9b5e5439d
iourivolkov <volk.iouri@gmail.com>
2021-11-07 05:15:50 +0000

    refactor breedFetcher.js to use callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,35 @@
-
 const request = require('request');
-const args = process.argv;
-// command line args to specify breed name --> array
-const breedRequest = args.slice(2);
-// breedname is an element of the breedRequest array
-const breedName = breedRequest[0];
-// assign url to variable
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-// request resource from the url
-request(url, (error, response, body) => {
-// if error occurs, function exits
-  if (error) {
-    console.log(error);
+
+// const args = process.argv;
+// const breedName = args.slice(2);
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  // request resource from the url
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+  // if error occurs, function exits
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    // JSON parse returns empty array --> if no value provided
+    if (data.length === 0) {
+      callback("This breed was not found.", null);
+      return;
+    }
+    // print element of data array, key = description
+    callback(null, data[0]["description"]);
     return;
-  }
-  const data = JSON.parse(body);
-  // JSON parse returns empty array --> if no value provided
-  if (data.length === 0) {
-    console.log("This breed was not found.");
-    return;
-  }
-  // print element of data array, key = description
-  console.log(data[0]["description"]);
-});
+  });
+};
+
+
+module.exports = {fetchBreedDescription};
+
+
+
 
 
 
